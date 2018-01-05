c859596c0d106e688d24f720c2535ecc4539f8f0
Helder Falcão <helderfalcaob@gmail.com>
2018-01-05 09:57:04 -0200

    Criação estrutura base no projeto.

---
+++ app/src/main/java/bgsong/com/br/dojokotlin/CreateMemberActivity.kt
@@ -0,0 +1,12 @@
+package bgsong.com.br.dojokotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CreateMemberActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_member)
+    }
+}
---
+++ app/src/main/java/bgsong/com/br/dojokotlin/DailyActivity.java
@@ -0,0 +1,8 @@
+package bgsong.com.br.dojokotlin;
+
+/**
+ * Created by falcao on 05/01/18.
+ */
+
+public class DailyActivity {
+}
