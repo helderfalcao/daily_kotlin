1634597f216965ea4b2b940e8ff98d4238c6777a
Helder da Silva Falcao <helder@ciandt.com>
2018-03-02 16:49:46 -0300

    Serialização member para preferences

---
+++ app/src/main/java/bgsong/com/br/dojokotlin/Constant.kt
@@ -0,0 +1,8 @@
+package bgsong.com.br.dojokotlin
+
+class Constants {
+    companion object {
+        @JvmStatic val SHARED_KEY = "shared_app"
+        @JvmStatic val MEMBERS_KEY = "members"
+    }
+}
