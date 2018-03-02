3837e11e34a3e41d53ed7c214f0be38a6e4b1a01
castromatth@gmail.com <matheusjc@ciandt.com>
2018-03-02 15:05:30 -0300

    altera Adapter

--- app/src/main/java/bgsong/com/br/dojokotlin/adapter/MembersAdapter.kt
+++ app/src/main/java/bgsong/com/br/dojokotlin/adapter/MembersAdapter.kt
@@ -4,13 +4,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import android.widget.TextView
 
-class MembersAdapter : RecyclerView.Adapter<MembersAdapter.ViewHolder>(memberList: List<Member>) {
-
+class MembersAdapter(val memberList: List<Member>)  : RecyclerView.Adapter<MembersAdapter.ViewHolder>(){
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -18,6 +13,12 @@ class MembersAdapter : RecyclerView.Adapter<MembersAdapter.ViewHolder>(memberLis
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    class ViewHolder(name: TextView) : RecyclerView.ViewHolder(name)
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(memberViewHolder: TextView) : RecyclerView.ViewHolder(memberViewHolder) {
+
+    }
 
 }
