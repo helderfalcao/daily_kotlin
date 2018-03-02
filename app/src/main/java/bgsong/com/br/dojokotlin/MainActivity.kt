3f84745f628b45b848296cafac5c739447cd77f0
lfuzzatti <lfuzzatti@ciandt.com>
2018-03-02 13:18:56 -0300

    Teste

--- app/src/main/java/bgsong/com/br/dojokotlin/MainActivity.kt
+++ app/src/main/java/bgsong/com/br/dojokotlin/MainActivity.kt
@@ -3,6 +3,9 @@ package bgsong.com.br.dojokotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ *
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
