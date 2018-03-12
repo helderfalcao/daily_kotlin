49cbc2c795955511b4db494118229953c9e70092
Stanislao <stanislao@ciandt.com>
2018-03-12 14:50:20 -0300

    boa sorte juba

--- app/src/main/java/bgsong/com/br/dojokotlin/adapter/MembersAdapter.kt
+++ app/src/main/java/bgsong/com/br/dojokotlin/adapter/MembersAdapter.kt
@@ -23,12 +23,28 @@ class MembersAdapter(private val memberList: List<Member>,
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1,
                 parent, false)
+        var holder = ViewHolder(view.findViewById(android.R.id.text1))
 
-        return ViewHolder(view.findViewById(android.R.id.text1))
+        holder.listen { pos, type ->
+            val item = memberList[pos]
+        }
+
+        return holder
     }
 
     class ViewHolder(memberViewHolder: TextView) : RecyclerView.ViewHolder(memberViewHolder) {
         val memberName = memberViewHolder
+    }
+
+    fun <T : RecyclerView.ViewHolder> T.listen(event: (position: Int, type: Int) -> Unit): T {
+        itemView.setOnClickListener {
+            event.invoke(adapterPosition, itemViewType)
+        }
+        return this
+    }
+
+    interface OnClickMember{
+
     }
 
 }
