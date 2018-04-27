dd082dbe0ca1d01ab1b3ab844cb31fca1ec6f785
Leticia Ponce <letfponce@gmail.com>
2018-04-27 13:26:16 -0300

    teste de commit

--- app/src/main/java/bgsong/com/br/dojokotlin/MainActivity.kt
+++ app/src/main/java/bgsong/com/br/dojokotlin/MainActivity.kt
@@ -5,9 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-/**
- *
- */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
