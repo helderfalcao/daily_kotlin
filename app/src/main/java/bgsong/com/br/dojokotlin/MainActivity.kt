41a1036b765e4c002900acdf6eb91c184d06fb5f
Raphael de Rocha Castro <raphaelc@lnxcit011445.cit>
2018-04-27 15:33:32 -0300

    ajusta método de validação; SEGURA A BOMBA LÊ

--- app/src/main/java/bgsong/com/br/dojokotlin/MainActivity.kt
+++ app/src/main/java/bgsong/com/br/dojokotlin/MainActivity.kt
@@ -1,8 +1,8 @@
 package bgsong.com.br.dojokotlin
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import bgsong.com.br.dojokotlin.utils.MemberUtils
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,35 +21,33 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, ListMembersActivity::class.java))
         }
         dailyButton.setOnClickListener {
-
-            if (MemberUtils.hasDevOrQa(MemberUtils.getMembersList(this))) {
-                Toast.makeText(this, "Nenhum dev ou QA inserido", Toast.LENGTH_LONG).show()
-            } else if (MemberUtils.hasArchitect(MemberUtils.getMembersList(this))) {
-                Toast.makeText(this, "Nenhum Arquiteto inserido", Toast.LENGTH_LONG).show()
-            } else if (MemberUtils.hasSm(MemberUtils.getMembersList(this))) {
-                Toast.makeText(this, "Nenhum SM inserido", Toast.LENGTH_LONG).show()
-            } else {
+            if (isValidList()) {
                 startActivity(Intent(this, DailyActivity::class.java))
             }
         }
     }
 
     private fun isValidList(): Boolean {
-        var isValid = true
-        var errorMessage : String
-        var memberList = MemberUtils.getMembersList(this)
+        var isValid = false
+        var errorMessage: String = ""
+        val memberList = MemberUtils.getMembersList(this)
         if (!MemberUtils.hasDevOrQa(memberList)) {
             errorMessage = getString(R.string.none_role_inserted,
                     getString(R.string.developer_or_qa))
-            isValid = false
-        } else if (MemberUtils.hasArchitect(memberList)) {
+        } else if (!MemberUtils.hasArchitect(memberList)) {
             errorMessage = getString(R.string.none_role_inserted,
                     getString(R.string.architect))
-        } else if (MemberUtils.hasSm(MemberUtils.getMembersList(this))) {
-            Toast.makeText(this, "Nenhum SM inserido", Toast.LENGTH_LONG).show()
+        } else if (!MemberUtils.hasSm(MemberUtils.getMembersList(this))) {
+            errorMessage = getString(R.string.none_role_inserted,
+                    getString(R.string.sm))
         } else {
-            startActivity(Intent(this, DailyActivity::class.java))
+            isValid = true
         }
-        return isValid;
+
+        if (!isValid) {
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+        }
+
+        return isValid
     }
 }
